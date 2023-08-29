c050bfba57fccceca92c91c2daa5f5c442f8015d
WantChane <wantchane@gmail.com>
2023-08-29 23:36:35 +0800

    7

--- semi-auto-green.bat
+++ semi-auto-green.bat
@@ -1,4 +1,4 @@
 cd E:\vscode\Program\semi-auto-green
 start python test.py
 set /p count=<./count.txt
-start "" "C:\Program Files\Git\git-bash.exe" -c "cd=E:\vscode\Program\semi-auto-green && git add .  && git commit -m %count%;bash"
+start "" "C:\Program Files\Git\git-bash.exe" -c "cd=E:\vscode\Program\semi-auto-green && git add .  && git commit -m %count% && git push;bash"
